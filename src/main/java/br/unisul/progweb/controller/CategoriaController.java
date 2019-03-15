cc543f393f131a9844c76d91440acfdec6cdbc72
Lucas <lucasnunes-tb@hotmail.com>
2019-03-14 22:19:35 -0300

    criando classe serverTest
    classe abstrata para configurações gerais de test. Adicionando as dependencias do modelMapper

--- src/main/java/br/unisul/progweb/controller/CategoriaController.java
+++ src/main/java/br/unisul/progweb/controller/CategoriaController.java
@@ -11,7 +11,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/categorias")
 public class CategoriaController {
-
+    
     @GetMapping
     public List<Categoria> listar() {
         return Collections.singletonList(Categoria.builder()
--- src/test/java/br/unisul/progweb/domain/categoria/controller/CategoriaControllerTest.java
+++ src/test/java/br/unisul/progweb/domain/categoria/controller/CategoriaControllerTest.java
@@ -1,12 +1,10 @@
 package br.unisul.progweb.domain.categoria.controller;
 
 
+import br.unisul.progweb.ProgWebApplicationServerTests;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
 
@@ -15,9 +13,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class CategoriaControllerTest {
+
+public class CategoriaControllerTest extends ProgWebApplicationServerTests {
 
     private MockMvc mockMvc;
 
@@ -37,3 +34,7 @@ public class CategoriaControllerTest {
     }
 
 }
+
+
+
+
